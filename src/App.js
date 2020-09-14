f0b888c8139053fbe3736127fd191aac27fddb0d
bwapes <benwaples@gmail.com>
2020-09-13 18:18:18 -0700

    fixed nav with toggle option

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ export default class App extends Component {
   }
 
   render() {
+
     return (
       <div className="stayCalm">
       <Router>
--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import e from 'cors';
 
 export default class extends Component {
+
+  state = {
+    displayNav: false
+  }
+
+  handleNav = () => {
+    const navBar = document.querySelector('nav')
+
+    if(this.state.displayNav) {
+    this.setState( {displayNav: false }) 
+    navBar.classList.remove('stretch')
+    } else {
+    this.setState( {displayNav: true })
+    navBar.classList.add('stretch')
+    }
+    
+  }
+
+  
+
   render() {
     return (
-      <header>
-        <img className="logo" src="newlogo.png" alt="pocd logo"/>
-        <h1>Nasa Photos of the Day</h1>
+      <>
+        <header>
+          <img className="logo" src="newlogo.png" alt="pocd logo"/>
+          <h1>NASA Photos of the Day</h1>
+        </header>
+        
         <nav>
-          <Link to="/TodaysPic">Today</Link>
-          <Link to="/Calendar">Calendar</Link>
-          <Link to="/FavPage">Your Favorites</Link>
-          <Link to="/">Logout</Link>
+        {
+          this.state.displayNav 
+          ?
+          <>
+            <Link to="/TodaysPic">Today</Link>
+            <Link to="/Calendar">Calendar</Link>
+            <Link to="/FavPage">Your Favorites</Link>
+            <Link to="/">Logout</Link>
+            <p onClick={this.handleNav}>Minimize</p>
+            </>
+          :
+          <img className="navIcon" src="./menuIcon.png" alt="toolbar" onClick={this.handleNav} />
+
+        }
         </nav>
-      </header>
+        
+      </>
     )
   }
 }
